b49cfaa5de8a08d881b12db79680bac2f218eb99
Joshua Green <joshuagreen@Joshuas-MacBook-Pro.local>
2024-11-29 18:02:26 +0900

    last version working in dev

--- app/tools/data-tool/page.tsx
+++ app/tools/data-tool/page.tsx
@@ -2,7 +2,7 @@ import MinimalBehaviorAnalyzer from '../../../components/BehaviourAnalyser';
 import NavBar from '../../../components/nav';
 export default function Home() {
   return (
-    <div className='flex flex-col gap-16'>
+    <div className='flex flex-col gap-12'>
       <header>
         <NavBar/>
       </header>
--- components/nav.tsx
+++ components/nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const NavBar = () => {
   return (
     <nav className="fixed top-0 left-0 bg-white/80 backdrop-blur-sm z-10 pl-0">
-      <div className="max-w-7xl mx-auto p-8">
+      <div className="max-w-7xl mx-auto p-4">
         <Link href="/" className="text-[#7EE66B] hover:text-[#6CD559]">
           ← Return
         </Link>
